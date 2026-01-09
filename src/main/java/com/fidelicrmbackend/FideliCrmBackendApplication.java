c1f85edf61dd3da134af0ce6c86c6e748f17df80
hugobardales <vhbardalesp@gmail.com>
2026-01-08 23:40:58 -0500

    Initial commit uploading the structure of the proyect with Spring Boot

---
+++ src/main/java/com/fidelicrmbackend/FideliCrmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fidelicrmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FideliCrmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FideliCrmBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fidelicrmbackend/FideliCrmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fidelicrmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FideliCrmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
